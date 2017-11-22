a02ee3623b9e82bda4477ccb84665238e1514f3f
zgq_story <zgq_story>
2017-11-22 13:19:02 +0800

    创建Look项目

---
+++ app/src/main/java/com/story/look/base/BaseView.java
@@ -0,0 +1,40 @@
+package com.story.look.base;
+
+/**
+ * Created by story on 2017/3/8 0008 下午 2:45.
+ * 基本视图接口
+ */
+public interface BaseView {
+
+    /**
+     * 展示进度框
+     * @param message 提示文字
+     * @param time 倒计时时间，小于等于0表示不需要
+     */
+    void showLoading(String message, int time);
+
+    /**
+     * 隐藏进度框
+     */
+    void hideLoading();
+
+    /**
+     * 弹出通用提示框
+     * @param message 显示内容
+     */
+    void alertMessage(String message);
+
+    /**
+     * Toast方式输入提示
+     * @param message 显示内容
+     */
+    void toastMessage(String message);
+
+    /**
+     * 获取strings文件中字符串
+     * @param id id
+     * @return string
+     */
+    String getStringById(int id);
+
+}
